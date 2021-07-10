5a3a8a5bae9705494a736e791dc435319e97f654
aK0nshin <kon2work@gmail.com>
2021-07-10 21:32:19 +0300

    Start all over

--- nuka-cola-vending-machine/coins.ino
+++
@@ -1,7 +0,0 @@
-void incCoinCounter() {
-  // инкрементируем счётчик количества рублей
-  coinsCount++;
-  // выводим текущий баланс
-  Serial.print(F("coinsCount: "));
-  Serial.println(coinsCount);
-}
